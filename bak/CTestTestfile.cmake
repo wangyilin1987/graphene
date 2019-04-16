d8f0bb3d3df0320399223bd7c20115d4fe501bdf
wangyilin <wang_yi_lin@163.com>
2019-04-16 15:16:53 +0800

    add by wyl

---
+++ bak/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/wangyilin/wyl/work/eos/eos
+# Build directory: /home/wangyilin/wyl/work/eos/eos
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(programs)
